71623099688ceecd1038cd9a897ba7c67c67b030
Kagiso Kgomo <kagisok@xpedia.co.za>
2021-09-21 17:34:12 +0200

    added routes temporaily, add established routes to app js file using withRouter Hoc

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.components.jsx';
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+
+  <div>
+  <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
       <HomePage/>
     </div>
+    /* 
+    <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/' component={HomePage}/>
+    </Switch>
+    
+    */
   );
 }
 
